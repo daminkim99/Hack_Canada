148b130eccdaf777ffc7034b6885ae995a37faee
Zumh <17211423+Zumh@users.noreply.github.com>
2025-02-22 16:13:56 -0500

    data base is done

--- models/User.js
+++ models/User.js
@@ -9,6 +9,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     address: { type: String, required: true },
+    foodsAmount: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now }
   })
 
