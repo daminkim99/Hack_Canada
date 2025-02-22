ec09fa050ffb1e5454c545eb581131b139830548
Damin (Elena) Kim <elenanana0903@gmail.com>
2025-02-22 08:48:56 -0500

    add routing to the frontend

---
+++ client/src/pages/Finder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Finder= () => {
+    return (
+        <h1>This is Finder.</h1>
+    )
+}
+
+export default Finder;
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LandingHero from '../../components/landingHero.jsx'
+
+const Landing= () => {
+    return (
+       <LandingHero />
+    )
+}
+
+export default Landing;
