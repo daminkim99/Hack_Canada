aa22e921cbf06fee4d0314c290dcac5b39841ad0
Damin (Elena) Kim <elenanana0903@gmail.com>
2025-02-22 13:07:17 -0500

    add UI for searching address

---
+++ client/components/finderChild.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import  Button  from "./button";
+import  Input  from "./input";
+
+const FinderChild = ({ address, setAddress, onSearch }) => {
+  return (
+    <div>
+      <Input
+        type="text"
+        placeholder="Enter your address"
+        value={address}
+        onChange={(e) => {setAddress(e.target.value);}} form control here
+      />
+      <Button onClick={onSearch}>Search</Button> {/* Calls parent function */}
+    </div>
+  );
+};
+
+export default FinderChild;
--- client/src/pages/Finder.jsx
+++ client/src/pages/Finder.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import FinderChild from "../../components/finderChild";
 
-const Finder= () => {
-    return (
-        <h1>This is Finder.</h1>
-    )
-}
+const Finder = () => {
+  const [address, setAddress] = useState("");
+
+  const handleSearch = () => {
+    if (!address.trim()) {
+      alert("Please enter an address");
+      return;
+    }
+    console.log("Searching for:", address);
+    // api request
+  };
+
+  return (
+    <div>
+      <h1>Address Finder</h1>
+      <FinderChild
+        address={address} 
+        setAddress={setAddress} 
+        onSearch={handleSearch} 
+      />
+    </div>
+  );
+};
 
 export default Finder;
