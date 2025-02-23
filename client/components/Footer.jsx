87439798d0917a39ac10e08f2fb1334d55145daa
hatha <144495047+nhinguyen277@users.noreply.github.com>
2025-02-22 19:40:03 -0500

    home page design

---
+++ client/components/Footer.jsx
@@ -0,0 +1,6 @@
+// import React from 'react'
+import "../src/styles/Footer.css";
+
+export default function Footer() {
+  return <div className="footer">© 2025 FoodShare</div>;
+}
---
+++ client/components/Header.jsx
@@ -0,0 +1,22 @@
+import logo from "../src/assets/logo.png";
+import "../src/styles/header.css";
+import PropTypes from "prop-types";
+
+export default function Header({ children }) {
+  return (
+    <header>
+      <div className="left">
+        <img src={logo} alt="Logo" />
+        <div className="brandName">
+          <h2>FoodShare</h2>
+          <p> Saving food and saving environment</p>
+        </div>
+      </div>
+      {children}
+    </header>
+  );
+}
+
+Header.propTypes = {
+  children: PropTypes.node,
+};
